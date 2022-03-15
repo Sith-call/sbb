1727e14b97cad46e4a705a438788e5355ccb552f
pahkey <pahkey@gmail.com>
2022-03-15 11:49:44 +0900

    2-07

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,15 +1,22 @@
 package com.mysite.sbb.question;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuestionController {
+	
+	@Autowired
+    private QuestionRepository questionRepository;
 
 	@RequestMapping("/question/list")
-	@ResponseBody
-	public String list() {
-		return "question list";
+	public String list(Model model) {
+		List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+		return "question_list";
 	}
 }
