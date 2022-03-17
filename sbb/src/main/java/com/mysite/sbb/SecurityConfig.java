5d2e821fa8a0d7505c5611139ad41703cb906ce9
pahkey <pahkey@gmail.com>
2022-03-17 11:41:19 +0900

    3-06

--- sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
+++ sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.mysite.sbb;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
@@ -20,4 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
         ;
 	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
