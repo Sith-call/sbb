fb37efdb8a306feaaca4fd8dc1ea18cad5633f25
pahkey <pahkey@gmail.com>
2022-03-27 12:01:45 +0900

    3-03

--- sbb/src/main/java/com/mysite/sbb/question/Question.java
+++ sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.question;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
